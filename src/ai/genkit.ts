fe66af071226382aafcf7f8c958805618e6f9948
Ksksja Jsjssjj <ksksjajsjssjj@gmail.com>
2025-09-19 11:47:50 +0000

    in my real website and in my github codesapce sign up and login works bu

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,5 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: "AIzaSyB6PgSGzpxbWEhNs9eFHP1iIX9n6ZvuVgE"})],
-  // model: 'googleai/gemini-2.0-flash', //This is not a valid model
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
 });
